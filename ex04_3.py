239b0203eaaa2d4e93a1a235dedfba18060747f3
kabuki7 <95139192+kabuki7@users.noreply.github.com>
2022-07-11 18:14:02 +0900

    Victory message added

---
+++ ex04_3.py
@@ -0,0 +1,18 @@
+import random
+
+coin = [3]*3
+tail = 0
+head = 0
+
+print("Tossing a coin...")
+for times in range(3) :
+	coin = random.randint(1, 1000)
+	if ( coin % 2 == 0 ) :
+		print("Round "+str(times)+":Head")
+		head += 1
+	else : 
+		print("Round "+str(times)+":Tail")
+		tail += 1
+print("Heads: "+str(head)+", Tails: "+str(tail))
+if ( head > tail ) : print("You won")
+else : print("You lost")
